1954769d4ae8bae2930eecd4a2f9260b81934f47
Sz <szalicil@gmail.com>
2014-11-01 19:36:57 +0100

    auto-open DuoBot translation

--- Duolingo widescreen translation.user.js
+++ Duolingo widescreen translation.user.js
@@ -11,6 +11,9 @@
 var root='#app.wiki-translation.wide ';
 var mroot=root + 'main.main-left ';
 var iroot=root + '.lesson-like-document.hide-instructions.view-original ';
+
+var mt='div.machine-translation';
+
 function addGlobalStyle(css) {
     var head, style;
     head = document.getElementsByTagName('head')[0];
@@ -33,3 +36,14 @@ addGlobalStyle(iroot + '.document-header-new { height: 72px; }');
 addGlobalStyle(iroot + '#document-omnibox { margin-top: 92px; }');
 addGlobalStyle(iroot + '#document-omnibox hr { display:none; }');
 
+$('body').on("focus","div.no-translation",function() {
+    var h4=$(this).find('h4.toggle-machine-translation');
+    if(h4.length===0) return;
+
+    var machine=$(this).find(mt);
+    if(machine.length>0) return;
+
+    if(!h4.hasClass("active")) {
+        h4.click();
+    }
+});
