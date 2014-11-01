d893750a5cefebd276bca852c217f42feb071dd9
Sz <szalicil@gmail.com>
2014-11-01 19:39:32 +0100

    add button to clone DuoBot translation into the textarea

--- Duolingo widescreen translation.user.js
+++ Duolingo widescreen translation.user.js
@@ -13,6 +13,7 @@ var mroot=root + 'main.main-left ';
 var iroot=root + '.lesson-like-document.hide-instructions.view-original ';
 
 var mt='div.machine-translation';
+var cmt="clone-machine-translation";
 
 function addGlobalStyle(css) {
     var head, style;
@@ -36,9 +37,27 @@ addGlobalStyle(iroot + '.document-header-new { height: 72px; }');
 addGlobalStyle(iroot + '#document-omnibox { margin-top: 92px; }');
 addGlobalStyle(iroot + '#document-omnibox hr { display:none; }');
 
+window.cloneDuobotTranslation=function(id) {
+	var div=$('div#'+id);
+
+    var machine=div.find(mt);
+    
+    var textarea=div.find("textarea.wiki-textarea");
+    if(textarea.text().length===0) {
+        textarea.text(machine.text());
+        return;
+    }
+    alert('textarea is not empty');
+}
+
 $('body').on("focus","div.no-translation",function() {
     var h4=$(this).find('h4.toggle-machine-translation');
     if(h4.length===0) return;
+
+    var container=$(this).find("div.wiki-textarea-container div:last");
+    if(container.find("."+cmt).length===0) {
+        container.append("<a href='javascript:window.cloneDuobotTranslation(\""+$(this).parent("div.document-sentence-sidebar").attr('id')+"\");' style='margin-left: 10px' class='"+cmt+" btn btn-standard' data-toggle='tooltip' title='' data-original-title='Clone DuoBot translation to the textarea' onclick=''>Clone</a>");
+    }
 
     var machine=$(this).find(mt);
     if(machine.length>0) return;
