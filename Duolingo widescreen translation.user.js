7024774c372b0f9819d55cc1e36e3f98278109bf
Sz <szalicil@gmail.com>
2014-11-01 19:33:32 +0100

    free up some horizontal space on the right sidebar

--- Duolingo widescreen translation.user.js
+++ Duolingo widescreen translation.user.js
@@ -10,6 +10,7 @@
 
 var root='#app.wiki-translation.wide ';
 var mroot=root + 'main.main-left ';
+var iroot=root + '.lesson-like-document.hide-instructions.view-original ';
 function addGlobalStyle(css) {
     var head, style;
     head = document.getElementsByTagName('head')[0];
@@ -27,4 +28,8 @@ addGlobalStyle(root + 'div#document-hider {width:100%;}');
 addGlobalStyle(mroot + '{max-width:100%; padding: 0 10px 0 10px;}');
 addGlobalStyle(mroot + 'section div.responsive-container {max-width:100%; }');
 addGlobalStyle(mroot + 'section div.document-header-new {width:98%;width:calc(100% - 20px); }');
+
+addGlobalStyle(iroot + '.document-header-new { height: 72px; }');
+addGlobalStyle(iroot + '#document-omnibox { margin-top: 92px; }');
+addGlobalStyle(iroot + '#document-omnibox hr { display:none; }');
 
